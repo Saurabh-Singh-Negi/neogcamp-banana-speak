2a030bc3a624513548d08aa214ee0d2136a0a580
Saurabh-Singh-Negi <saurabhnegi0007@gmail.com>
2022-03-27 17:29:57 +0530

    textarea input and console

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var text = document.querySelector("#text");
 
 btnTranslate.addEventListener("click", function onClick() {
-    console.log("clicked");
+    console.log("clicked!");
+    console.log("you entered:", text.value);
 })
