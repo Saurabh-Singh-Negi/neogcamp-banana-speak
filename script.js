ebc9e95ef8c5bd5208a20e9f628f159b3d3e4001
Saurabh-Singh-Negi <saurabhnegi0007@gmail.com>
2022-03-28 11:54:24 +0530

    mock api call

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ var btnTranslate = document.querySelector("#btn-translate");
 var inputText = document.querySelector("#text-area");
 var outputText = document.querySelector("#output");
 
+var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
-btnTranslate.addEventListener("click", function onClicking(){
-    outputText.innerText = inputText.value + " hahahaha";
+function getCompleteUrl(text) {
+    return serverUrl + "?" + "text=" + text;
+}
+
+
+btnTranslate.addEventListener("click", function onClick(){
+    var textByUser = inputText.value;
+
+    fetch(getCompleteUrl(textByUser))
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated));
+
 });
 
