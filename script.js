cc7018778ddcaa967f8bbf652fc3663d39b8bd35
Saurabh-Singh-Negi <saurabhnegi0007@gmail.com>
2022-03-28 13:17:48 +0530

    Added Minion API

--- script.js
+++ script.js
@@ -2,19 +2,29 @@ var btnTranslate = document.querySelector("#btn-translate");
 var inputText = document.querySelector("#text-area");
 var outputText = document.querySelector("#output");
 
-var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+//var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
 
 function getCompleteUrl(text) {
     return serverUrl + "?" + "text=" + text;
 }
 
+function errorHandler(error) {
+    console.log("you have an error",error);
+}
 
-btnTranslate.addEventListener("click", function onClick(){
+function clickHandler() {
+    
     var textByUser = inputText.value;
-
+    //server call for processing 
     fetch(getCompleteUrl(textByUser))
     .then(response => response.json())
-    .then(json => console.log(json.contents.translated));
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputText.innerText = translatedText;
+    })
+    .catch(errorHandler)
 
-});
+};
 
+btnTranslate.addEventListener("click", clickHandler);
